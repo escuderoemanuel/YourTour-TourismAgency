c11e51f92a7d00c53fbb0327879f8bad1ce1f842
Emanuel Escudero <escudero.mdz@gmail.com>
2024-07-02 18:27:24 -0300

    Updated: logo on header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,12 +3,11 @@
 import React, { useState, useEffect } from 'react';
 import styles from './Header.module.css';
 import Image from 'next/image';
-const logo = 'https://i.ibb.co/7rcr25z/airplane.png';
 import NavLinks from '../NavLinks/NavLinks';
 import LanguageSelector from '../LanguageSelector/LanguageSelector';
 import content from '../../translations/es/global.json';
 // Importar fuentes
-import { praise, raleway, raleway600 } from '../../ui/fonts';
+import { raleway } from '../../ui/fonts';
 
 const Header = () => {
   const [isScrolled, setIsScrolled] = useState(false);
@@ -29,19 +28,14 @@ const Header = () => {
     ${isScrolled ? 'glassBackground' : 'bg-sky-700'} 
     ${raleway.className} `}>
       <div className='max-w-screen-xl mx-auto flex flex-row justify-between items-center'>
-        <div className={`flex items-center text-center `}>
-          <Image
-            className=''
-            src={logo}
-            width={60}
-            height={60}
-            alt="Picture of the author"
-          />
-          <div className={`flex-col text-start ms-2 `}>
-            <h1 className={`text-3xl ${praise.className}  -mb-2.5 ${isScrolled ? 'text-primary-color' : 'text-white-color'}`}>{content.header.titleHeader}</h1>
-            <h3 className={`text-sm font-bold ${raleway600.className} ps-4 ${isScrolled ? 'text-primary-color' : 'text-white-color'}`}>{content.header.subtitleHeader}</h3>
-          </div>
-        </div>
+
+        <Image
+          src={`${isScrolled ? content.logo.yourTourPrimaryLogo : content.logo.yourTourSecondaryLogo}`}
+          width={250}
+          height={150}
+          alt="Picture of the author"
+        />
+
         <NavLinks isScrolled={isScrolled} />
         <LanguageSelector isScrolled={isScrolled} />
       </div>
