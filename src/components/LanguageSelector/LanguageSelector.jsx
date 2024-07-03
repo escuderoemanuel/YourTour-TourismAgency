20d1f7acbed15e824964ff4d746eeb5de7558a52
Emanuel Escudero <escudero.mdz@gmail.com>
2024-07-03 19:52:08 -0300

    Updated: Selected Language

--- src/components/LanguageSelector/LanguageSelector.jsx
+++ src/components/LanguageSelector/LanguageSelector.jsx
@@ -7,13 +7,10 @@ const LanguageSelector = ({ isScrolled }) => {
       <form className="max-w-sm mx-auto w-fit
       ">
         <select id="underline_select" className="block py-0.5 px-0 text-center rounded-md bg-transparent border-1 border-white appearance-none focus:outline-none focus:ring-0 focus:border-white peer w-fit">
-          <option
-            className={`text-primary-color text-sm w-fit`}
-            selected>{content.languages.language}</option>
           <option value="US"
             className={`text-primary-color text-sm`}
           >{content.languages.english}</option>
-          <option value="SP"
+          <option selected value="SP"
             className={`text-primary-color text-sm`}
           >{content.languages.spanish}</option>
           <option value="PR"
