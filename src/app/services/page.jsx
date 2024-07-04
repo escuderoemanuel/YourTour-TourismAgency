1dfc463413f2dd0d796827a8339b226bb2a7c0ca
Emanuel Escudero <escudero.mdz@gmail.com>
2024-07-04 03:43:16 -0300

    Added: Services Section

--- src/app/services/page.jsx
+++ src/app/services/page.jsx
@@ -1,12 +1,26 @@
 import React from 'react'
+import content from '../../translations/es/global.json'
+import Card from '@/components/Card/Card'
+
 
 export default function services() {
+  const listServices = Object.values(content.services.listServices)
   return (
     <section className={`min-h-screen align-middle p-4 sm:p-6 md:p-8 lg:p-14`}>
 
-      <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Blanditiis, placeat tempora voluptates nulla dolore, accusantium consequuntur, rerum reiciendis quibusdam autem delectus. Dolores dolor necessitatibus officia, asperiores fugiat in minima veniam.
-      </p>
-    </section>
+      <div className='flex flex-col md:flex-row md:flex-wrap items-center justify-center'>
+        {
+          listServices.map((service, index) => (
+            <Card
+              key={index}
+              alt={service.image.alt}
+              src={service.image.url}
+              title={service.title}
+              description={service.description}
+            />
+          ))
+        }
+      </div>
+    </section >
   )
 }
