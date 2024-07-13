e7aab92421ef74a2835fda580517dd7d37437a6f
Emanuel Escudero <escudero.mdz@gmail.com>
2024-07-13 17:29:39 -0300

    Updated

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,33 +1,29 @@
-
 import React from 'react'
 import Footer from '@/components/Footer/Footer';
 import Header from '@/components/Header/Header';
 import Whatsapp from '@/components/Whatsapp/Whatsapp';
 import "./globals.css";
 import { inter } from '../ui/fonts';
-
+import Head from 'next/head'; // Importar Head de Next.js
 
 const favicon = '/favicon.ico'
-
 
 export const metadata = {
   icons: {
     icon: favicon,
   },
-  title: "Your Tour ● Turism Agency",
-  description: "Turism Agency Website located in Mendoza, Argentina",
+  title: "Your Tour ● Tourism Agency",
+  description: "Tourism Agency Website located in Mendoza, Argentina",
 };
 
 export default function RootLayout({ children }) {
-
-
   return (
     <html lang="en">
-      <head>
+      <Head>
         <link rel="icon" href={metadata.icons.icon} />
         <title>{metadata.title}</title>
         <meta name="description" content={metadata.description} />
-      </head>
+      </Head>
       <body className={`${inter.className} antialiased bg-neutral-100`}>
         <Header />
         {children}
