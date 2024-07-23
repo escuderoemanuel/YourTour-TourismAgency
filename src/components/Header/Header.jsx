3542f6580562f2386e6b33308e6b85f8299f81c6
Emanuel Escudero <escudero.mdz@gmail.com>
2024-07-23 14:11:30 -0300

    Testing Logo Loading

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,12 +3,11 @@
 import React, { useState, useEffect } from 'react';
 import Image from 'next/image';
 import NavLinks from '../NavLinks/NavLinks';
+import Link from 'next/link';
 import { useTranslations } from 'next-intl';
-
 
 export default function Header() {
   const t = useTranslations('Header');
-
   const [isScrolled, setIsScrolled] = useState(false);
 
   useEffect(() => {
@@ -22,19 +21,26 @@ export default function Header() {
     };
   }, []);
 
+  // Limpia las rutas de las imágenes para evitar espacios adicionales
+  const primaryLogoSrc = t('primaryLogo').trim();
+  const secondaryLogoSrc = t('secondaryLogo').trim();
 
   return (
     <section className={`py-3 px-4 sticky z-50 top-0 md:py-4 md:px-12 lg:py-6 lg:px-24 ${isScrolled ? 'glassBackground' : 'bgHeader'}`}>
       <div className='max-w-screen-xl mx-auto flex flex-row justify-between items-start lg:items-center'>
-        <Image
-          className="relative w-44 h-10 sm:w-56 md:w-64"
-          src={isScrolled ? `${t('yourTourPrimaryLogo')} ` : `${t('yourTourSecondaryLogo')} `}
-          fill
-          sizes="(max-width: 256px)"
-          style={{ objectFit: 'contain' }}
-          alt="Your Tour Logo"
-          priority={true}
-        />
+        <Link href="/" className="relative w-44 h-10 sm:w-56 md:w-64">
+          <Image
+            src={isScrolled ? primaryLogoSrc : secondaryLogoSrc}
+            fill
+            sizes="(max-width: 256px)"
+            style={{ objectFit: 'contain' }}
+            alt="Your Tour Logo"
+            priority={true}
+          />
+        </Link>
+        {/* Verifica si las rutas se están generando correctamente */}
+        <p>{primaryLogoSrc}</p>
+        <p>{secondaryLogoSrc}</p>
         <NavLinks isScrolled={isScrolled} />
       </div>
     </section>
