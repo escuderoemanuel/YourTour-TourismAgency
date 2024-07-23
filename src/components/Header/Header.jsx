20c554c0a4e8be3c1fbe65adde0aa2535f1f94e5
Emanuel Escudero <escudero.mdz@gmail.com>
2024-07-23 13:25:31 -0300

    Testing Logo Loading

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,7 +3,6 @@
 import React, { useState, useEffect } from 'react';
 import Image from 'next/image';
 import NavLinks from '../NavLinks/NavLinks';
-import Link from 'next/link';
 import { useTranslations } from 'next-intl';
 
 
@@ -27,16 +26,15 @@ export default function Header() {
   return (
     <section className={`py-3 px-4 sticky z-50 top-0 md:py-4 md:px-12 lg:py-6 lg:px-24 ${isScrolled ? 'glassBackground' : 'bgHeader'}`}>
       <div className='max-w-screen-xl mx-auto flex flex-row justify-between items-start lg:items-center'>
-        <Link href="/" className="relative w-44 h-10 sm:w-56 md:w-64" >
-          <Image
-            src={isScrolled ? `${t('yourTourPrimaryLogo')} ` : `${t('yourTourSecondaryLogo')} `}
-            fill
-            sizes="(max-width: 256px)"
-            style={{ objectFit: 'contain' }}
-            alt="Your Tour Logo"
-            priority={true}
-          />
-        </Link>
+        <Image
+          className="relative w-44 h-10 sm:w-56 md:w-64"
+          src={isScrolled ? `${t('yourTourPrimaryLogo')} ` : `${t('yourTourSecondaryLogo')} `}
+          fill
+          sizes="(max-width: 256px)"
+          style={{ objectFit: 'contain' }}
+          alt="Your Tour Logo"
+          priority={true}
+        />
         <NavLinks isScrolled={isScrolled} />
       </div>
     </section>
