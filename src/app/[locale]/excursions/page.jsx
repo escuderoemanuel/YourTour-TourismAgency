c69e0683213b9ddf5b1ab6997f303dac90eee4f3
Emanuel Escudero <escudero.mdz@gmail.com>
2024-07-23 20:42:47 -0300

    Added Cabalgata Excursion & Updated Carousel

--- src/app/[locale]/excursions/page.jsx
+++ src/app/[locale]/excursions/page.jsx
@@ -1,18 +1,11 @@
 'use client';
 
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import Image from 'next/image';
 import { useTranslations } from 'next-intl';
 
 export default function Excursions() {
   const t = useTranslations('ExcursionsPage');
-
-  // Flowbite requiere useEffect para inicializar en el cliente
-  useEffect(() => {
-    if (typeof window !== 'undefined') {
-      import('flowbite');
-    }
-  }, []);
 
   // 'raw' obtiene los datos anidados sin que sean procesados como una cadena JSON
   const excursionsData = t.raw('listExcursions');
@@ -25,30 +18,9 @@ export default function Excursions() {
   return (
     <section className="max-w-screen-lg mx-auto min-h-screen p-4 sm:p-6 md:p-8 lg:p-14 2xl:max-w-screen-2xl">
       <div className="flex flex-col items-center md:items-stretch md:flex-row md:flex-wrap md:justify-center">
-
         {listExcursions.map((excursion, index) => (
           <div key={index} className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 mb-6 md:mx-4">
-            <div id="default-carousel" className="relative w-full z-40" data-carousel="slide">
-              {/* Carousel wrapper */}
-              <div className="relative aspect-w-4 aspect-h-3 sm:aspect-w-4 sm:aspect-h-3 md:aspect-w-16 md:aspect-h-9 overflow-hidden rounded-t-lg">
-                {excursion.images.map((image, imageIndex) => (
-                  <div
-                    key={imageIndex}
-                    className="duration-500 ease-in-out inset-0 w-full h-full"
-                    data-carousel-item
-                  >
-                    <Image
-                      alt={image.alt}
-                      src={image.url}
-                      fill
-                      sizes="(max-width: 500px)"
-                      loading="lazy"
-                      className="absolute inset-0 w-full h-full object-cover"
-                    />
-                  </div>
-                ))}
-              </div>
-            </div>
+            <ExcursionCarousel images={excursion.images} />
             <div className="p-5">
               <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{excursion.title}</h5>
               <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{excursion.description}</p>
@@ -57,5 +29,45 @@ export default function Excursions() {
         ))}
       </div>
     </section>
+  );
+}
+
+function ExcursionCarousel({ images }) {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+    }, 3000); // Cambia de imagen cada 3 segundos
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, [images.length]);
+
+  return (
+    <div className="relative w-full z-40">
+      {/* Carousel wrapper */}
+      <div className="relative aspect-w-4 aspect-h-3 sm:aspect-w-4 sm:aspect-h-3 md:aspect-w-16 md:aspect-h-9 overflow-hidden rounded-t-lg">
+        {images.map((image, index) => (
+          <div
+            key={index}
+            className={`duration-300 ease-in-out absolute inset-0 w-full h-full transition-opacity ${index === currentIndex ? 'opacity-100' : 'opacity-0'
+              }`}
+          >
+            <Image
+              alt={image.alt}
+              src={image.url}
+              fill
+              sizes="(max-width: 500px) 100vw, (max-width: 768px) 100vw, (max-width: 1024px) 50vw, 33vw"
+              quality={100} // Ajusta la calidad a 100
+              style={{ objectFit: 'cover' }}
+              priority={index === 0}
+              loading={index === 0 ? 'eager' : 'lazy'}
+            />
+          </div>
+        ))}
+      </div>
+    </div>
   );
 }
--- src/components/HomeCarousel/HomeCarousel.jsx
+++ src/components/HomeCarousel/HomeCarousel.jsx
@@ -1,59 +1,69 @@
 'use client';
 
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import Image from 'next/image';
 import { useTranslations } from 'next-intl';
 
 export default function HomeCarousel() {
   const t = useTranslations('HomePage');
 
-  // Flowbite requiere useEffect para inicializar en el cliente
-  useEffect(() => {
-    if (typeof window !== 'undefined') {
-      import('flowbite');
-    }
-  }, []);
-
   // Obtén las imágenes del homeCarousel
   const carouselImages = t.raw('homeCarousel');
   const imagesArray = Object.values(carouselImages);
 
-  return (
-    <>
-      {/* CAROUSEL */}
-      <div id="default-carousel" className="relative w-full z-40" data-carousel="slide">
-        {/* Carousel wrapper */}
-        <div className="relative aspect-w-4 aspect-h-3 sm:aspect-w-4 sm:aspect-h-3 md:aspect-w-16 md:aspect-h-9 overflow-hidden rounded-lg">
-          {imagesArray.map((image, index) => (
-            <div key={index} className="duration-700 ease-in-out absolute inset-0 w-full h-full" data-carousel-item>
-              <Image
-                alt={image.alt}
-                src={image.url}
-                fill
-                sizes="(max-width: 640px) 100vw, (max-width: 768px) 100vw, (max-width: 1024px) 50vw, 33vw"
-                style={{ objectFit: 'cover' }}
-                quality={100}
-                priority={index === 0}
-                loading={index === 0 ? 'eager' : 'lazy'}
-              />
-            </div>
-          ))}
-        </div>
+  const [currentIndex, setCurrentIndex] = useState(0);
 
-        {/* Slider indicators */}
-        <div className="absolute z-30 flex -translate-x-1/2 bottom-5 left-1/2 space-x-3 rtl:space-x-reverse">
-          {imagesArray.map((_, index) => (
-            <button
-              key={index}
-              type="button"
-              className="w-3 h-3 rounded-full"
-              aria-current={index === 0 ? 'true' : 'false'}
-              aria-label={`Slide ${index + 1}`}
-              data-carousel-slide-to={index}
-            ></button>
-          ))}
-        </div>
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % imagesArray.length);
+    }, 3000); // Cambia de imagen cada 3 segundos
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, [imagesArray.length]);
+
+  const handleIndicatorClick = (index) => {
+    setCurrentIndex(index);
+  };
+
+  return (
+    <div id="custom-carousel" className="relative w-full z-40">
+      {/* Carousel wrapper */}
+      <div className="relative aspect-w-4 aspect-h-3 sm:aspect-w-4 sm:aspect-h-3 md:aspect-w-16 md:aspect-h-9 overflow-hidden rounded-lg">
+        {imagesArray.map((image, index) => (
+          <div
+            key={index}
+            className={`duration-700 ease-in-out absolute inset-0 w-full h-full transition-opacity ${index === currentIndex ? 'opacity-100' : 'opacity-0'
+              }`}
+          >
+            <Image
+              alt={image.alt}
+              src={image.url}
+              fill
+              sizes="(max-width: 640px) 100vw, (max-width: 768px) 100vw, (max-width: 1024px) 50vw, (min-width: 1025px) 100vw"
+              quality={100} // Ajusta la calidad a 100
+              style={{ objectFit: 'cover' }}
+              priority={index === 0}
+              loading={index === 0 ? 'eager' : 'lazy'}
+            />
+          </div>
+        ))}
       </div>
-    </>
+
+      {/* Slider indicators */}
+      <div className="absolute z-30 flex -translate-x-1/2 bottom-5 left-1/2 space-x-3 rtl:space-x-reverse">
+        {imagesArray.map((_, index) => (
+          <button
+            key={index}
+            type="button"
+            className={`w-3 h-3 rounded-full ${index === currentIndex ? 'bg-gray-800' : 'bg-gray-400'}`}
+            aria-current={index === currentIndex ? 'true' : 'false'}
+            aria-label={`Slide ${index + 1}`}
+            onClick={() => handleIndicatorClick(index)}
+          ></button>
+        ))}
+      </div>
+    </div>
   );
 }
