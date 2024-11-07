c41feb440a964d399c7a88624f86a6fa762bc500
Emanuel Escudero <escudero.mdz@gmail.com>
2024-11-07 09:57:24 -0300

    Added: location in the footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -8,34 +8,28 @@ import { useTranslations } from 'next-intl';
 export default function Footer() {
   const t = useTranslations('Footer');
   return (
-    <section className={`flex flex-col py-6 px-4 lg:px-16 items-center justify-between bgFooter`}>
-      <div className='flex flex-col items-center gap-2' >
-        <div className={`max-w-screen-xl flex justify-between items-center gap-2 md:gap-4`}>
+    <section className={`flex flex-col pt-6 pb-2 px-4 lg:px-16 items-center justify-between bgFooter`}>
+
+      {/* Owner */}
+      <div className='flex flex-col items-center gap-2 md:flex-row md:justify-around md:w-full' >
+        <div className={`max-w-screen-xl flex justify-between items-center`}>
           <Image
-            className={`w-40 h-auto md:w-44`}
+            className={`w-48 h-auto sm:mb-4 md:w-56`}
             src={t('owner.logo.yourTourSecondaryLogo')}
             width={150}
             height={70}
             alt="Your Tour Logo"
-            loading="lazy"
+            loading="lazy" />
 
-          />
+        </div>
+        <div className={`max-w-screen-xl flex justify-between items-center gap-2 md:gap-4 mb-4`}>
           <Link
             href={t('owner.instagram')}
             target='_blank'
             rel='noopener noreferrer'
             title='Instagram'
             aria-label='Instagram'
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width={24}
-              height={24}
-              viewBox="0 0 24 24"
-              className='w-7 h-auto fill-gray-100 hover:origin-center  hover:scale-x-110 ease-in-out duration-200'
-            >
-              <path d="M11.999 7.377a4.623 4.623 0 1 0 0 9.248 4.623 4.623 0 0 0 0-9.248zm0 7.627a3.004 3.004 0 1 1 0-6.008 3.004 3.004 0 0 1 0 6.008z" />
-              <circle cx="16.806" cy="7.207" r="1.078" />
+          ><svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24" className='w-7 h-auto fill-gray-100 hover:origin-center  hover:scale-x-110 ease-in-out duration-200'> <path d="M11.999 7.377a4.623 4.623 0 1 0 0 9.248 4.623 4.623 0 0 0 0-9.248zm0 7.627a3.004 3.004 0 1 1 0-6.008 3.004 3.004 0 0 1 0 6.008z" />              <circle cx="16.806" cy="7.207" r="1.078" />
               <path d="M20.533 6.111A4.605 4.605 0 0 0 17.9 3.479a6.606 6.606 0 0 0-2.186-.42c-.963-.042-1.268-.054-3.71-.054s-2.755 0-3.71.054a6.554 6.554 0 0 0-2.184.42 4.6 4.6 0 0 0-2.633 2.632 6.585 6.585 0 0 0-.419 2.186c-.043.962-.056 1.267-.056 3.71 0 2.442 0 2.753.056 3.71.015.748.156 1.486.419 2.187a4.61 4.61 0 0 0 2.634 2.632 6.584 6.584 0 0 0 2.185.45c.963.042 1.268.055 3.71.055s2.755 0 3.71-.055a6.615 6.615 0 0 0 2.186-.419 4.613 4.613 0 0 0 2.633-2.633c.263-.7.404-1.438.419-2.186.043-.962.056-1.267.056-3.71s0-2.753-.056-3.71a6.581 6.581 0 0 0-.421-2.217zm-1.218 9.532a5.043 5.043 0 0 1-.311 1.688 2.987 2.987 0 0 1-1.712 1.711 4.985 4.985 0 0 1-1.67.311c-.95.044-1.218.055-3.654.055-2.438 0-2.687 0-3.655-.055a4.96 4.96 0 0 1-1.669-.311 2.985 2.985 0 0 1-1.719-1.711 5.08 5.08 0 0 1-.311-1.669c-.043-.95-.053-1.218-.053-3.654 0-2.437 0-2.686.053-3.655a5.038 5.038 0 0 1 .311-1.687c.305-.789.93-1.41 1.719-1.712a5.01 5.01 0 0 1 1.669-.311c.951-.043 1.218-.055 3.655-.055s2.687 0 3.654.055a4.96 4.96 0 0 1 1.67.311 2.991 2.991 0 0 1 1.712 1.712 5.08 5.08 0 0 1 .311 1.669c.043.951.054 1.218.054 3.655 0 2.436 0 2.698-.043 3.654h-.011z" />
             </svg>
           </Link>
@@ -76,22 +70,22 @@ export default function Footer() {
             </svg>
           </Link>
         </div>
-        <p className='text-xs text-secondary-color font-light'>
-          {t('owner.copyright')}
+        <div className='flex flex-col items-center md:items-end'>
+          <p className='text-xs text-secondary-color'>
+            {t('owner.copyright')}
+          </p>
+          <p className='text-xs text-secondary-color '>Mendoza - Argentina</p>
           <Link
-            className='ms-2 text-xs font-light underline decoration-1'
+            className='text-xs text-secondary-color underline decoration-1 font-extralight'
             href={'https://yourtour.ar/sitemap.xml'}
             target='_blank'
-            rel='noopener noreferrer'
-          >
-            Sitemap
-          </Link>
-        </p>
+            rel='noopener noreferrer'>Sitemap</Link>
+        </div>
       </div>
 
       {/* DEV */}
       <div className='max-w-screen-xl flex justify-between items-center gap-2 pt-8'>
-        <p className='text-xs text-secondary-color opacity-90 text-center'>{t('dev.paragraph')}</p>
+        <p className='text-xs text-secondary-color opacity-90 text-center font-extralight'>{t('dev.paragraph')}</p>
         <Link
           href={t('dev.devUrl')}
           target='_blank'
